a8c626d710fb000a31dd840485ec4bdc3e2f1147
Nuno M <greven.pt@gmail.com>
2018-01-26 02:05:31 +0000

    Fix migrations for adhoc execution

--- server/config/knexfile.js
+++ server/config/knexfile.js
@@ -1,19 +1,19 @@
 const path = require('path')
-const ROOT = path.resolve(__dirname, '../')
+const ROOT = path.resolve(__dirname, '../../')
 require('dotenv').config({ path: path.join(ROOT, '.env') })
 
 const { DB_CLIENT, DB_CONNECTION } = process.env
 
 const options = {
   client: DB_CLIENT || 'sqlite3',
-  connection: DB_CONNECTION || path.join(ROOT, 'data/dev.sqlite3'),
+  connection: DB_CONNECTION || path.join(ROOT, 'server/data/dev.sqlite3'),
   migrations: {
-    directory: path.join(ROOT, 'core/db/migrations'),
+    directory: path.join(ROOT, 'server/core/db/migrations'),
     tableName: 'migrations'
   },
   debug: false,
   seeds: {
-    directory: path.join(ROOT, 'core/db/seeds')
+    directory: path.join(ROOT, 'server/core/db/seeds')
   },
   useNullAsDefault: !DB_CLIENT || DB_CLIENT === 'sqlite3'
 }
