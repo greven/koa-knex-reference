128f83a2fd8c8dad7b7255225ea2dfc0dd4a5819
Nuno M <greven.pt@gmail.com>
2018-01-30 00:21:08 +0000

    Change to arrow function

--- server/middleware/auth.js
+++ server/middleware/auth.js
@@ -1,6 +1,7 @@
 const { UnauthorizedError } = require('../lib/errors')
 
-module.exports = function (ctx, next) {
+// Check if we have a logged in user
+module.exports = (ctx, next) => {
   if (!ctx.state.user) {
     ctx.throw(401, new UnauthorizedError())
   }
