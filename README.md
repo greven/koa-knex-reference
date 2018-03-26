37871c0e24561ed5be9d6650dfcb0c464d804ae7
Nuno Freire <greven.pt@gmail.com>
2018-03-26 01:21:03 +0100

    Clean slate

--- README.md
+++
@@ -1,2 +0,0 @@
-# ifly
-A simple concept app in NodeJS and VueJS to keep track of my flights
--- server/routes.js
+++
@@ -1,8 +0,0 @@
-const Router = require('koa-router')
-const router = new Router()
-
-const api = require('./api')
-
-router.use(api)
-
-module.exports = router.routes()
