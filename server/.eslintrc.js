876961d36a004d33526afde1f33c0acff75e66ce
Nuno M <greven.pt@gmail.com>
2018-05-14 02:01:46 +0100

    🚧

---
+++ server/.eslintrc.js
@@ -0,0 +1,13 @@
+module.exports = {
+  'env': {
+    'es6': true,
+    'browser': true,
+    'node': true,
+    'jest': true
+  },
+  'parserOptions': {
+    'ecmaVersion': 2017
+  },
+  'extends': 'airbnb',
+  'rules': { }
+}
